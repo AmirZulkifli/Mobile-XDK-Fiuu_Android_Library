d11c0128720e524efbd60daa414cc803556dc07c
Ashraf Amin <ashraf.amin@razer.com>
2025-06-12 10:32:24 +0800

    Added commented webcore testing server url

--- paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
+++ paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
@@ -227,6 +227,7 @@ public class MOLPayActivity extends AppCompatActivity {
         cookieManager.setAcceptThirdPartyCookies(mpMOLPayUI, true);
 
         mpMainUI.loadUrl("https://pay.merchant.razer.com/RMS/API/xdk/");
+//        mpMainUI.loadUrl("https://apps.apis17.net/ashraf/xdkwebcore/");
 
         // Configure MOLPay ui
         mpMOLPayUI.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
